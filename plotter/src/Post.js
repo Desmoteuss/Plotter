2df73d9b0b8451e1362adf94eda455957125378f
desmoteuss <desmoteuss@gmail.com>
2020-08-29 16:48:28 +0200

    adding post feater

--- plotter/src/Post.js
+++ plotter/src/Post.js
@@ -1,38 +1,40 @@
-import React from 'react'
+import React from 'react';
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import"./Post.css";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
+import Avatar from '@material-ui/core/Avatar'
 
 const Post= ({displayName,userName,verified,text,image,avatar} )=> {
     return (
         <div className="post">
             <div className="post__avatar">
-                {/* <Avatar srv={avatar}/> */}
+                 <Avatar srv={avatar}/> 
 
             </div>  
             <div className="post__body">
                 <div className="post__header">
                     <div className="post_headerText">
                         <h3>
-                            trolokopter
+                            {displayName}
                             <span className="post__headerSpecial">
-                            <VerifiedUserIcon className="post__badge"/>
+    {verified&&<VerifiedUserIcon className="post__badge"/> }@{userName}
                             </span>
                         </h3>
 
                     </div>
-                    <div className="header__Description">
-                        <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                    <div className="post__headerDescription">
+                        <p> {text}</p>
+                        
 
 
-                        </p>
+                     
 
                     </div>
                 </div>
-            <image src="https://static.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg"> </image>
+            <image src= {image}> </image>
                                 <div className="post__footer">
                                 <ChatBubbleOutlineIcon fontSize="small"/>
                                 <RepeatIcon  fontSize="small"/>
