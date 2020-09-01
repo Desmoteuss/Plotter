8ddd0138ecd3e61beaf38e34f3573210de47bd7b
desmoteuss <desmoteuss@gmail.com>
2020-09-01 23:04:34 +0200

    adding firebase

--- plotter/src/PlottBox.js
+++ plotter/src/PlottBox.js
@@ -8,7 +8,7 @@ const PlottBox= () => {
         <div className='plottBox'> 
         <form>
 <div className='plottBox__input'>
-<Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+<Avatar />
 <input placeholder= "How are u ?" type="text"/>
 
 
